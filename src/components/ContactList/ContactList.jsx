388f81e00658eb73df3dd9ee7858fd0f6b4a7408
Taras Yarosevych <121568027+jarastar@users.noreply.github.com>
2023-05-04 18:05:06 +0300

    Initial commit

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types';
+import ContactItem from 'components/ContactItem/ContactItem';
+import { List } from './ContactList.styled';
+
+const ContactList = ({ contacts, deleteContact }) => (
+  <List>
+    {contacts.map(({ id, name, number }) => (
+      <ContactItem
+        key={id}
+        id={id}
+        name={name}
+        number={number}
+        deleteItem={deleteContact}
+      />
+    ))}
+  </List>
+);
+
+export default ContactList;
+
+ContactList.defaultProps = {
+  contacts: [],
+};
+
+ContactList.propTypes = {
+  deleteContact: PropTypes.func.isRequired,
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
+};
---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+import { FilterWrap, FilterInput } from './Filter.styled';
+
+const Filter = ({ handleChange }) => (
+  <FilterWrap>
+    <label>Find contacts by name</label>
+    <FilterInput type="text" name="filter" onChange={handleChange} />
+  </FilterWrap>
+);
+
+export default Filter;
+
+Filter.propTypes = {
+  handleChange: PropTypes.func.isRequired,
+};
